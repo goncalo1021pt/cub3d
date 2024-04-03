7c94500f06c68a2a49d9683a7dc4def7b248e04b
goncalo1021pt <goncalo.pereira.1021@gmail.com>
2024-04-03 23:42:49 +0100

    auto

--- srcs/start.c
+++ srcs/start.c
@@ -6,21 +6,6 @@ bool	start_game(t_map *map)
 
 	create_grid(map);
 	initialize_player(&player, map);
-	// print_grid(map->grid);
-	ft_printf("\n");
-	ft_printf("move_angle: %d\n", player.angle);
-	// rotate_player(&player, RIGHT_ARROW);
-	rotate_player(&player, RIGHT_ARROW);
-	ft_printf("move_angle: %d\n", player.angle);
-	move_player(&player, map, 1, EAST);
-	// move_player(&player, map, 1, EAST);
-	// move_player(&player, map, 1, EAST);
-	// move_player(&player, map, 1, NORTH);
-	// move_player(&player, map, 1, NORTH);
-	// move_player(&player, map, 1, NORTH);
-	// move_player(&player, map, 1, NORTH);
-	// move_player(&player, map, 1, NORTH);
-	// move_player(&player, map, 1, NORTH);
 	print_grid(map->grid);
 	return (true);
 }
