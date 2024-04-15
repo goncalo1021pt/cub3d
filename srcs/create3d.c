41bd4736610dbfb4349ae7743ca6d1353dbb65fa
goncalo1021pt <goncalo.pereira.1021@gmail.com>
2024-04-15 15:11:49 +0100

    auto

---
+++ srcs/create3d.c
@@ -0,0 +1,17 @@
+#include "../includes/headers/cub3d.h"
+
+void cast_3d(t_session *instance)
+{
+	int x;
+	int y;
+
+	y = (W_HEIGHT / 2) -1;
+	while (++y < (W_HEIGHT / 2))
+	{
+		x = -1;
+		while (++x != W_WIDTH)
+		{
+			pixel_put(&(instance->mlx_img), x, y, 0x7FFF00);
+		}
+	}
+}
