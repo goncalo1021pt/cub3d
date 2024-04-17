2bf144a9a8048d808a3a76788cb9c904036d8d99
iuri- <123178126+iurilandolt@users.noreply.github.com>
2024-04-17 16:25:32 +0100

    auto

--- srcs/camera3D.c
+++ srcs/camera3D.c
@@ -35,87 +35,102 @@ typedef struct s_slice
 	int	color;
 } t_slice;
 
+double	clamp_ray(double dir)
+{
+	if (dir == 0)
+		return (W_WIDTH);
+	else
+		return (fabs(1 / dir));
+}
+
+void	init_camera3D(t_session *instance, t_camera3D *camera)
+{
+	camera->dir_x = cos(instance->player.angle * (PI / 180));
+	camera->dir_y = sin(instance->player.angle * (PI / 180));
+	camera->fov = 72 * (PI / 180); // Field of view in radians
+	camera->plane_x = -camera->dir_y * tan(camera->fov / 2);
+	camera->plane_y = camera->dir_x * tan(camera->fov / 2);
+}
+
+void	init_ray(t_camera3D *canera, t_ray *ray, int i, double pos_x, double pos_y)
+{
+	ray->x = (int)pos_x;
+	ray->y = (int)pos_y;
+	canera->x = 2 * i / (double)W_WIDTH - 1;
+	ray->ray_dir_x = canera->dir_x + canera->plane_x * canera->x;
+	ray->ray_dir_y = canera->dir_y + canera->plane_y * canera->x;
+	ray->delta_dist_x = clamp_ray(ray->ray_dir_x);
+	ray->delta_dist_y = clamp_ray(ray->ray_dir_y);
+}
+
+void	aim_ray(t_ray *ray)
+{
+	if (ray->ray_dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (ray->x - ray->x) * ray->delta_dist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->x + 1.0 - ray->x) * ray->delta_dist_x;
+	}
+	if (ray->ray_dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (ray->y - ray->y) * ray->delta_dist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->y + 1.0 - ray->y) * ray->delta_dist_y;
+	}
+}
+
+void	cast_ray(t_session *instance, t_ray	*ray)
+{
+	int	hit;
+
+	hit = 0;
+	while (hit == 0)
+	{
+		if (ray->side_dist_x < ray->side_dist_y)
+		{
+			ray->side_dist_x += ray->delta_dist_x;
+			ray->x += ray->step_x;
+			ray->side = 0;
+		}
+		else
+		{
+			ray->side_dist_y += ray->delta_dist_y;
+			ray->y += ray->step_y;
+			ray->side = 1;
+		}
+		//pixel_put(&instance->mlx_img, ray->x, ray->y, 0xFF0000); // check ray in 2Debug
+		if (instance->map.grid[ray->y][ray->x] == '1')
+			hit = 1;
+	}
+	if (ray->side == 0)
+		ray->perp_wall_dist = (ray->side_dist_x - ray->delta_dist_x);
+	else
+		ray->perp_wall_dist = (ray->side_dist_y - ray->delta_dist_y);
+}
+
+
 void	camera3D(t_session *instance, double pos_x, double pos_y)
 {
-
 	t_camera3D	camera;
 	t_ray		ray;
 	t_slice		slice;
 	int	i;
 
-	// setup camera
-	camera.dir_x = cos(instance->player.angle * (PI / 180));
-	camera.dir_y = sin(instance->player.angle * (PI / 180));
-	camera.fov = 72 * (PI / 180); // Field of view in radians
-	camera.plane_x = -camera.dir_y * tan(camera.fov / 2);
-	camera.plane_y = camera.dir_x * tan(camera.fov / 2);
-
+	init_camera3D(instance, &camera);
 	i = 0;
 	while (i < W_WIDTH) //i < n_rays
 	{
-		// setup values for dda
-		ray.x = (int)pos_x;
-		ray.y = (int)pos_y;
-		camera.x = 2 * i / (double)W_WIDTH - 1;
-		ray.ray_dir_x = camera.dir_x + camera.plane_x * camera.x;
-		ray.ray_dir_y = camera.dir_y + camera.plane_y * camera.x;
-
-		if (ray.ray_dir_y == 0)
-			ray.delta_dist_x = W_WIDTH;
-		else
-			ray.delta_dist_x = fabs(1 / ray.ray_dir_x);
-
-		if (ray.ray_dir_x == 0)
-			ray.delta_dist_y = W_WIDTH;
-		else
-			ray.delta_dist_y = fabs(1 / ray.ray_dir_y);
-
-		if (ray.ray_dir_x < 0)
-		{
-			ray.step_x = -1;
-			ray.side_dist_x = (pos_x - ray.x) * ray.delta_dist_x;
-		}
-		else
-		{
-			ray.step_x = 1;
-			ray.side_dist_x = (ray.x + 1.0 - pos_x) * ray.delta_dist_x;
-		}
-		if (ray.ray_dir_y < 0)
-		{
-			ray.step_y = -1;
-			ray.side_dist_y = (pos_y - ray.y) * ray.delta_dist_y;
-		}
-		else
-		{
-			ray.step_y = 1;
-			ray.side_dist_y = (ray.y + 1.0 - pos_y) * ray.delta_dist_y;
-		}
-		// exec dda until collision
-		ray.hit = 0;
-		while (ray.hit == 0)
-		{
-			if (ray.side_dist_x < ray.side_dist_y)
-			{
-				ray.side_dist_x += ray.delta_dist_x;
-				ray.x += ray.step_x;
-				ray.side = 0;
-			}
-			else
-			{
-				ray.side_dist_y += ray.delta_dist_y;
-				ray.y += ray.step_y;
-				ray.side = 1;
-			}
-			//pixel_put(&instance->mlx_img, x, y, 0xFF0000); // check ray in 2Debug
-			if (instance->map.grid[ray.y][ray.x] == '1')
-				ray.hit = 1;
-		}
-		if (ray.side == 0)
-			ray.perp_wall_dist = (ray.side_dist_x - ray.delta_dist_x);
-		else
-			ray.perp_wall_dist = (ray.side_dist_y - ray.delta_dist_y);
-
-		//render vertical slices from left to right
+		init_ray(&camera, &ray, i, pos_x, pos_y);
+		aim_ray(&ray);
+		cast_ray(instance, &ray);
 		if (ray.perp_wall_dist > 0)
 		{
 			slice.height = (int)(W_HEIGHT / ray.perp_wall_dist * MAP_SCALE);
@@ -129,7 +144,7 @@ void	camera3D(t_session *instance, double pos_x, double pos_y)
 			if (ray.side == 1)
 				slice.color = 0x2f4f4f;
 			draw_line(instance, (t_point){i, slice.start}, (t_point){i, slice.end}, slice.color);
-			}
-			i++;
+		}
+		i++;
 	}
 }
